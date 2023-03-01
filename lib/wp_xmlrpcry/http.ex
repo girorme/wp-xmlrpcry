ad05a073e1f19f47ce0ad4fc2a2d1074d784a9be
girorme <rodrigogirorme@gmail.com>
2023-03-01 02:24:32 -0300

    ignore ssl validations

--- lib/wp_xmlrpcry/http.ex
+++ lib/wp_xmlrpcry/http.ex
@@ -1,6 +1,8 @@
 defmodule WpXmlrpcry.Http do
   def post(url, payload) do
-    HTTPoison.post(url, payload, [{"Content-Type", "application/xml"}])
+    HTTPoison.post(url, payload, [{"Content-Type", "application/xml"}],
+      hackney: [:insecure]
+    )
   end
 
   def text_in_body?(body, pattern), do: String.match?(body, pattern)
