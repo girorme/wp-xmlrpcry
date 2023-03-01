df5a4ba860c0ef3d1b6357865b48f17616f3cb63
girorme <rodrigogirorme@gmail.com>
2023-03-01 02:25:34 -0300

    add debug data

--- .iex.exs
+++ .iex.exs
@@ -3,3 +3,14 @@ work = fn ->
   IO.puts("Sleeping for #{seconds}")
   Process.sleep(:timer.seconds(seconds))
 end
+
+# debug
+results = [
+  %{
+    credentials: [],
+    credentials_found?: false,
+    total_tries: 4,
+    url: "https://www.site.com/xmlrpc.php"
+  }
+]
+opt = [time_lapsed: "23 seconds", output: "output.txt"]
